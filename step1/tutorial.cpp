ec4da238150b3ca8ea9ec9b47cc2d643d4aaa395
pansf <pansf@xiaopeng.com>
2023-11-28 18:33:53 +0800

    compatible linux and windows

--- step1/tutorial.cpp
+++ step1/tutorial.cpp
@@ -1,14 +1,16 @@
 #include "project_config.h"
 #include "MathFunctions.h"
 
+#if defined(_WIN32) && defined(_MSC_VER)
+#include <string>
+#endif
 
 #include <iostream>
-
 
 int main(int argc, char *argv[])
 {
     printf("Hello, CMake!\n");
-    printf("version: %d.%d.%d\n", PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);    
+    printf("version: %d.%d.%d\n", PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
     const double inputValue = std::stod(argv[1]);
     const double outputValue = mathfunctions::sqrt(inputValue);
     printf("the sqrt of %g is %g\n", inputValue, outputValue);
